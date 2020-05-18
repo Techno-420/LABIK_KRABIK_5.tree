ec974357bd126fe8ad800e29821a228b4f4eb940
Techno-420 <54204064+Techno-420@users.noreply.github.com>
2020-05-18 21:09:35 +0300

    Update LABIK_KRABIK_5.py

--- LABIK_KRABIK_5.py
+++ LABIK_KRABIK_5.py
@@ -1,1 +1,11 @@
-print('Hello, world')
+class Expression:
+    def __init__(self):
+        self.expression = []
+    def reading(self,name):
+        expr = open(name, 'r')
+        for lines in expr:
+            self.expression.append(lines)
+        for i in range(len(self.expression) - 1):
+            self.expression[i] = self.expression[i][:-1]
+a = Expression()
+a.reading('expression.txt')
