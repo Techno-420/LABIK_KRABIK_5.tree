537076c98cf41f330f9e4a870ba0044112a9930f
Techno-420 <54204064+Techno-420@users.noreply.github.com>
2020-05-20 23:57:34 +0300

    Parse tree

--- LABIK_KRABIK_5.py
+++ LABIK_KRABIK_5.py
@@ -2,7 +2,8 @@ class Expression:
     def __init__(self):
         self.expression = []
         self.variables = {}
-    def reading(self,name):
+        self.tree = {}
+    def reading(self, name):
         expr = open(name, 'r')
         for lines in expr:
             self.expression.append(lines)
@@ -17,36 +18,60 @@ class Expression:
                     self.expression[i][j] = self.expression[i][j][1:]
                     self.expression[i].insert(0, "\t")
         print(self.expression)
+
     def init_var(self):
         temp_arr = []
         for i in range(len(self.expression)):
-                if "=" in self.expression[i]:
-                    temp_arr.append([self.expression[i][0],self.expression[i][2]])
-        self.variables = {temp_arr[i][0]:float(temp_arr[i][1]) for i in range(len(temp_arr))}
+            if "=" in self.expression[i]:
+                temp_arr.append([self.expression[i][0], self.expression[i][2]])
+        self.variables = {temp_arr[i][0]: float(temp_arr[i][1]) for i in range(len(temp_arr))}
 
         print(self.variables)
-    def operations(self,operator,a,b):
-        operation = [a*b, a/b, a+b, a-b, a**b]
-        operators = ['*','/','+','-','^']
-        operation_dict = {operators[i]:operation[i] for i in range(len(operators))}
-        print('operation dict',operation_dict)
+
+    def operations(self, operator, a, b):
+        operation = [a * b, a / b, a + b, a - b, a ** b]
+        operators = ['*', '/', '+', '-', '^']
+        operation_dict = {operators[i]: operation[i] for i in range(len(operators))}
         if operator in operation_dict:
             return operation_dict.get(operator)
-    def count_expression(self):
+    def parse_tree(self):
+        temp_str = ' '
         for el in self.expression:
-            print(el)
             if ("\t" not in el) and ("def" not in el) and ("=" not in el):
-                temp_v=[]
-                for k in el:
-                    if k in self.variables.keys():
-                        temp_v.append(self.variables.get(k))
-                    elif k.isdigit():
-                        temp_v.append(float(k))
-                print(temp_v)
-                print(self.operations(el[1],temp_v[0],temp_v[1]))
-    
+                temp_str = temp_str.join(el)
+                self.tree = {temp_str:el for i in range(len(el))}
+                print(self.tree)
+        arr_items = self.tree.get(temp_str)
+        self.count_expression([arr_items])
+
+    def count_expression(self,expression):
+
+        for el in expression:
+            if len(el)>3:
+                el1 = el[:3]
+                if ("\t" not in el1) and ("def" not in el1) and ("=" not in el1):
+                    temp_v = []
+                    for k in el:
+                        if k in self.variables.keys():
+                            temp_v.append(self.variables.get(k))
+                        elif k not in ['*', '/', '^', '+', '-']:
+                            temp_v.append(float(k))
+                    el = el[3:]
+                    self.variables['el1'] = self.operations(el1[1],temp_v[0],temp_v[1])
+                    el.insert(0,self.variables.get('el1'))
+                    self.count_expression([el])
+            else:
+                if ("\t" not in el) and ("def" not in el) and ("=" not in el):
+                    temp_v = []
+                    for k in el:
+                        if k in self.variables.keys():
+                            temp_v.append(self.variables.get(k))
+                        elif k not in ['*', '/', '^', '+', '-']:
+                            temp_v.append(float(k))
+                    print(self.operations(el[1], temp_v[0], temp_v[1]))
 
 a = Expression()
 a.reading('expression.txt')
 a.init_var()
-a.count_expression()
+#a.count_expression(a.expression)
+a.parse_tree()
